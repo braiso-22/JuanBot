fe99a669778e2823a38f8995f2f32d2f41d4b61c
Brais <braisfv22@gmail.com>
2025-02-02 01:55:29 +0100

    updated intents

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,17 @@ package com.braiso_22
 
 import com.braiso_22.mensamientos.mensamiento
 import com.braiso_22.mensamientos.randomMensamiento
+import com.braiso_22.saludation.saludation
 import dev.kord.common.entity.PresenceStatus
 import dev.kord.core.Kord
 import dev.kord.gateway.Intent
+import dev.kord.gateway.Intents
 import dev.kord.gateway.PrivilegedIntent
 import dev.kord.gateway.builder.PresenceBuilder
 
+
 suspend fun main() {
+
     val kord = Kord("")
     kord.saludation()
     kord.help()
@@ -18,7 +22,7 @@ suspend fun main() {
     kord.login {
         // we need to specify this to receive the content of messages
         @OptIn(PrivilegedIntent::class)
-        intents += Intent.MessageContent
+        intents += Intents(Intent.GuildMembers, Intent.MessageContent)
 
         // esto es para enseñar una luna y el juego en el estado
         presence = PresenceBuilder().apply {
